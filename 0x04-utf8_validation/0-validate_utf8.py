a9053e339d1b80caaa6aac017f96bc5465ae433c
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-09 22:35:43 +0100

    utf8-validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+A method that determines if a given data set
+represents a valid UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    A method that determines if a given data set
+    represents a valid UTF-8 encoding.
+    """
+    bytes_remaining = 0
+
+    for byte in data:
+        if bytes_remaining == 0:
+            if byte >> 7 == 0b0:
+                continue
+            elif byte >> 5 == 0b110:
+                bytes_remaining = 1
+            elif byte >> 4 == 0b1110:
+                bytes_remaining = 2
+            elif byte >> 3 == 0b11110:
+                bytes_remaining = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            bytes_remaining -= 1
+
+    return bytes_remaining == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+## Learning utf8 Validation
