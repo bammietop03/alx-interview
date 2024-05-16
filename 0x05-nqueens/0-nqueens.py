858828265b8f0b6e84d6ab7d5daa83c56d2bdabc
Bamiwo Adebayo <bammietop@gmail.com>
2024-05-16 23:13:33 +0200

    N queens3

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,73 +1,81 @@
 #!/usr/bin/python3
-""" N QUEENS ALGORITHM WITH BACKTRACKING (RECURSION INSIDE LOOP) """
+"""
+This module solves the N queens problem. It uses backtracking algorithm to
+place N non-attacking queens on an N×N chessboard
+"""
 import sys
 
 
-class NQueen:
-    """ Class for solving N Queen Problem """
+def is_safe(board, row, col):
+    """
+    Check if it's safe to place a queen at the given row and column.
 
-    def __init__(self, n):
-        """ Global Variables """
-        self.n = n
-        self.x = [0 for i in range(n + 1)]
-        self.res = []
+    Parameters:
+    - board (list): List representing the board where board[i] is the column of
+                    the queen in row i.
+    - row (int): The row to check.
+    - col (int): The column to check.
 
-    def place(self, k, i):
-        """ Checks if k Queen can be placed in i column (True)
-        or if the are attacking queens in row or diagonal (False)
-        """
-
-        # j checks from 1 to k - 1 (Up to previous queen)
-        for j in range(1, k):
-            # There is already a queen in column
-            # or a queen in same diagonal
-            if self.x[j] == i or \
-               abs(self.x[j] - i) == abs(j - k):
-                return 0
-        return 1
-
-    def nQueen(self, k):
-        """ Tries to place every queen in the board
-        Args:
-        k: starting queen from which to evaluate (should be 1)
-        """
-        # i goes from column 1 to column n (1st column is 1st index)
-        for i in range(1, self.n + 1):
-            if self.place(k, i):
-                # Queen can be placed in i column
-                self.x[k] = i
-                if k == self.n:
-                    # Placed all 4 Queens (A solution was found)
-                    solution = []
-                    for i in range(1, self.n + 1):
-                        solution.append([i - 1, self.x[i] - 1])
-                    self.res.append(solution)
-                else:
-                    # Need to place more Queens
-                    self.nQueen(k + 1)
-        return self.res
+    Returns:
+    - bool: True if it's safe to place the queen, False otherwise.
+    """
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
 
 
-# Main
+def solve_nqueens(n, row, board, solutions):
+    """
+    Solve the N-Queens problem using backtracking.
 
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
+    Parameters:
+    - n (int): The size of the board (N x N).
+    - row (int): The current row to place a queen.
+    - board (list): List representing the board where board[i] is the column of
+                    the queen in row i.
+    - solutions (list): List to collect the solutions.
+    """
+    if row == n:
+        solutions.append([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col):
+            board[row] = col
+            solve_nqueens(n, row + 1, board, solutions)
 
-N = sys.argv[1]
 
-try:
+def nqueens(N):
+    """
+    Validate the input, initialize the board, and solve the N-Queens problem.
+
+    Parameters:
+    - N (str): The size of the board as a string.
+    """
+    if not N.isdigit():
+        print("N must be a number")
+        sys.exit(1)
     N = int(N)
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
 
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
+    solutions = []
+    board = [-1] * N
+    solve_nqueens(N, 0, board, solutions)
 
-queen = NQueen(N)
-res = queen.nQueen(1)
+    for solution in solutions:
+        print(solution)
 
-for i in res:
-    print(i)
+
+if __name__ == "__main__":
+    """
+    Entry point of the program. Validates the command-line arguments and
+    calls nqueens function.
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    nqueens(sys.argv[1])
